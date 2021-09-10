2a5572e930f496aaafd7cbbf821286c1d4b9805a
ankur <ankurkapil8@gmail.com>
2021-09-10 13:03:46 +0530

    changes

--- controllers/EMIs.js
+++ controllers/EMIs.js
@@ -21,9 +21,8 @@ app.post("/calculateEMI", async(req, res, next) => {
           message: validationResult.error.details
         });        
       }
-      console.log(req.body.loanStartDate);
       return res.status(200).json({
-        message: calculateEMIFlat(req.body.loan_amount, req.body.tenure, req.body.interest_rate, req.body.EMI_payout, new Date(req.body.loanStartDate))
+        message: calculateEMIFlat(req.body.loan_amount, req.body.tenure, req.body.interest_rate, req.body.EMI_payout, new Date(req.body.loanStartDate), req.body.week, req.body.day)
       });
     }catch (error) {
         return res.status(500).json({
@@ -32,7 +31,49 @@ app.post("/calculateEMI", async(req, res, next) => {
       }
 
 })
- function calculateEMIFlat(totalLoan, tenure, interest_rate, EMI_payout, loanDate){
+  function calculateFirstEmiVillage(week,day,application_date = moment()){
+    let currentMonthArrayByDay = {"sunday":[],"monday":[],"tuesday":[],"wednesday":[],"thrusday":[],"friday":[],"saturday":[]};
+    let nextMonthArrayByDay = {"sunday":[],"monday":[],"tuesday":[],"wednesday":[],"thrusday":[],"friday":[],"saturday":[]};
+    let firstEmiDate = "";
+    let monthStartDate = moment(application_date).startOf('month');
+    let monthEndDate = moment(application_date).endOf('month');
+
+    let nextMonthStartDate = moment(application_date).add(1, 'month').startOf('month');
+    let nextMonthEndDate = moment(application_date).add(1, 'month').endOf('month');
+    let dayByNumber = {0:"sunday",1:"monday",2:"tuesday",3:"wednesday",4:"thrusday",5:"friday",6:"saturday"}; 
+    
+    for(let i=monthStartDate;i<=monthEndDate;i=moment(i).add(1, 'day')){ //list date by day for current month
+      currentMonthArrayByDay[dayByNumber[moment(i).day()]].push(i);
+    }
+    for(let i=nextMonthStartDate;i<=nextMonthEndDate;i=moment(i).add(1, 'day')){ //list date by day for next month
+      nextMonthArrayByDay[dayByNumber[moment(i).day()]].push(i);
+    }
+    let i = (week-1);
+    while(i<currentMonthArrayByDay[day].length){ //check in current month
+      if(currentMonthArrayByDay[day][i] > application_date){  // Emi can not start from application date, so we are using > operator
+        firstEmiDate = currentMonthArrayByDay[day][i];
+        break;
+      }
+      i = i+2;
+    }
+
+    if( firstEmiDate == ""){ //because emi date is not available in current month
+      i = (week-1);
+      while(i<nextMonthArrayByDay[day].length){ // check in next month
+        if(nextMonthArrayByDay[day][i] > application_date){  // Emi can not start from application date, so we are using > operator
+          firstEmiDate = nextMonthArrayByDay[day][i];
+          break;
+        }
+        i = i+2;
+      }
+  
+    }
+
+    return firstEmiDate;
+  }
+ function calculateEMIFlat(totalLoan, tenure, interest_rate, EMI_payout, loanDate, week=0, day=""){
+   try{
+      
   let result = [];
   let totalIntAmount = Math.ceil(totalLoan*interest_rate/100);
   let intPerTenure =  Math.ceil(totalIntAmount/tenure);
@@ -41,16 +82,21 @@ app.post("/calculateEMI", async(req, res, next) => {
   //let currentEMI = 0;
   let totalAmount = totalLoan+totalIntAmount;
   let EMIPerTenure = totalAmount/tenure;
-  console.log("totalAmount "+totalAmount);
-  console.log("totalIntAmount "+totalIntAmount);
-
+  let firstEmiDate = "";
+  if(EMI_payout == "village"){
+    firstEmiDate = calculateFirstEmiVillage(week, day, loanDate);
+  }
+  console.log("firstEmiDate ",firstEmiDate);
+  let w = 0
   for(let i=1;i<=tenure;i++){
     if(EMI_payout=="monthly"){
       nextEMIDate = new Date(loanDate.setMonth(loanDate.getMonth()+1))
     }else if(EMI_payout=="weekly"){
-        nextEMIDate = new Date(loanDate.setDate(loanDate.getDate() + 1 * 7));
+      nextEMIDate = new Date(loanDate.setDate(loanDate.getDate() + 1 * 7));
     }else if(EMI_payout=="fortnight"){
-        nextEMIDate = new Date(loanDate.setDate(loanDate.getDate()+15))
+      nextEMIDate = new Date(loanDate.setDate(loanDate.getDate()+15))
+    }else if(EMI_payout=="village"){
+      nextEMIDate = moment(firstEmiDate).add(w, 'weeks');
     }
     outstanding = outstanding-principalPerTenure;
      let emi = {
@@ -62,8 +108,12 @@ app.post("/calculateEMI", async(req, res, next) => {
          "remain_EMI":tenure-i
         }
         result.push(emi);
+        w = w+2;
   }
   return result;
+  }catch(error){
+    console.log(error);
+  }
 }
 app.get("/dueEMIs/:dueDate", async(req, res, next) => {
   try {
