987a6204fae52437ad40eb41c3e5b6f1b967b396
ankurkapil8 <ankurkapil8@gmail.com>
2022-07-02 14:10:04 +0530

    changes

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var Q = require('q');
 var mysql = require('mysql');
+
 const connectionParams={
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -11,26 +12,48 @@ const connectionParams={
 //   password : 'JxtIHMlWKJPDawlnjnqs',
 //   database: 'mydb'
 // });
- 
-var connection = mysql.createConnection({
+var connection;
+ const db_config = {
   host     : 'sg2plzcpnl479109.prod.sin2.secureserver.net',
   user     : 'aa2_admin',
   password : 'JxtIHMlWKJPDawlnjnqs',
   database: 'aa2_dev'
-});
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+};
+// var connection = mysql.createConnection({
+//   host     : 'sg2plzcpnl479109.prod.sin2.secureserver.net',
+//   user     : 'aa2_admin',
+//   password : 'JxtIHMlWKJPDawlnjnqs',
+//   database: 'aa2_dev'
+// });
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
  
+//   console.log('mysql connected as id ' + connection.threadId);
+// });
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
   console.log('mysql connected as id ' + connection.threadId);
-});
+}
+
+handleDisconnect();
 module.exports = connection;
-//require('./models/CategoryModel');
-//require('./models/UserModel');
-//require('./models/FinanceModel');
-//require('./models/OrderModel');
-//require('./models/ProductModel');
-//require('./models/ShoppingCartModel');
-//add other models here.
