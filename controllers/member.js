235a9e7a024237bcf0f423f1ff7d63374d358960
ankurkapil8 <ankurkapil8@gmail.com>
2022-07-10 21:58:16 +0530

    changes

--- controllers/member.js
+++ controllers/member.js
@@ -37,13 +37,15 @@ app.get("/entry/:member_id", async(req, res, next) => {
           message: validationResult.error.details
         });        
       }
+      req.body.date_of_birth = moment(req.body.date_of_birth).format('yyyy-MM-DD');
+      req.body.enrollment_date = moment(req.body.enrollment_date).format('yyyy-MM-DD');
+
       var formatedData = {
         status:0,
         ...req.body
       }
       try{
-        req.body.date_of_birth = moment(req.body.date_of_birth).format('yyyy-MM-DD');
-        req.body.enrollment_date = moment(req.body.enrollment_date).format('yyyy-MM-DD');
+        console.log(req.body);
         let response = await MemberModel.save(formatedData);
         return res.status(200).json({
             message: response
